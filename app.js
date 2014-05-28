ad0ff4c9d0fbd4fec1af619661a398b5263bbd70
vichid <victor.hidalgo.lorenzo@gmail.com>
2014-05-28 18:21:16 +0100

    Added contact form and express

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var nodemailer = require("nodemailer");
+var app = express();
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/dist'));     // set the static files location /public/img will be /img for users
+    app.use(express.logger('dev'));             // log every request to the console
+    app.use(express.json());
+    app.use(express.urlencoded());
+  });
+
+app.get('*', function(req, res) {
+  res.sendfile('index.html');
+});
+
+app.post('/', function(req, res) {
+  console.log(req.body);
+  var smtpTransport = nodemailer.createTransport("SMTP",{
+      service: "Gmail",
+      auth: {
+          user: "email",
+          pass: "password"
+      }
+  });
+
+  var mailOptions = {
+      from: "email",
+      to: "email",
+      subject: "Portfolio",
+      html: "Name: <b>" + req.body.name + "</b><br>Body: " + req.body.message + "<br>Email:" + req.body.email
+  }
+
+  smtpTransport.sendMail(mailOptions, function(error, response){
+      if(error){
+          res.send(500, 'Something broke');
+      }else{
+          res.redirect('/');
+      }
+  });
+});
+
+app.listen(3001);
+console.log("App listening on port 3001");
--- app/scripts/controllers/main.coffee
+++ app/scripts/controllers/main.coffee
@@ -1,9 +1,6 @@
 'use strict'
 
 angular.module('portfolioApp')
-  .controller 'MainCtrl', ($scope) ->
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate'
-      'AngularJS'
-      'Karma'
-    ]
+  .controller 'MainCtrl', ($scope,$http,$location) ->
+    $scope.contact = {};
+
